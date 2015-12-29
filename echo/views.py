5bc34b37410dbff094b19a2b3afaaf4f78504fd3
Jax Wang <451478064@qq.com>
2015-12-29 22:03:52 +0800

    完成接入微信公众平台

--- echo/views.py
+++ echo/views.py
@@ -1,8 +1,24 @@
+import hashlib
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    #return HttpResponse('hello, Jax')
-    return render(request, 'index.html', {})
+    if request.method == 'GET':
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        echostr = request.GET.get('echostr')
+        print 'signature ' + signature
+
+        token = 'wx192'
+        args = [token, timestamp, nonce]
+        args.sort()
+        encrypt_signature = hashlib.sha1(args[0] + args[1] + args[2]).hexdigest()
+        print 'Signature ' + encrypt_signature
+
+        if signature == encrypt_signature:
+            print echostr
+            return HttpResponse(echostr)
